6692567f2164b800ebb1ba3e346d122f82b024c3
Roxana Angheluta <roxana.angheluta@xenit.eu>
2019-05-23 15:07:13 +0200

    Working version - but the original function does not have a File parameter and without it does not work.

--- src/main/java/testConnectHTTPJod/App.java
+++ src/main/java/testConnectHTTPJod/App.java
@@ -21,104 +21,176 @@ import org.alfresco.service.cmr.repository.TransformationOptions;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.entity.mime.content.StringBody;
 
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+	public String getGreeting() {
+		return "Hello world.";
+	}
 
-    private static final String DEFAULT_JODCONVERTER_ENDPOINT = "http://localhost:5050/converter";
-    private static final Log logger = LogFactory.getLog(App.class);
-    private static final List<String> TARGET_MIMETYPES = Arrays
-            .asList(new String[] { MimetypeMap.MIMETYPE_TEXT_PLAIN, MimetypeMap.MIMETYPE_PDF });
-    private static String endpoint = DEFAULT_JODCONVERTER_ENDPOINT;
+	private static final String DEFAULT_JODCONVERTER_ENDPOINT = "http://localhost:8080/converter";
+	private static final Log logger = LogFactory.getLog(App.class);
+	private static final List<String> TARGET_MIMETYPES = Arrays
+			.asList(new String[] { MimetypeMap.MIMETYPE_TEXT_PLAIN, MimetypeMap.MIMETYPE_PDF });
+	private static String endpoint = DEFAULT_JODCONVERTER_ENDPOINT;
 
-    public static void main(String[] args) throws Exception {
-       ContentReader reader = new FileContentReader(new File("/home/jasper/Downloads/test.vsd"));
-        File file = new File("/home/jasper/Downloads/outputTest.vsd");
-        transformInternal(reader,new FileContentWriter(file),new RuntimeExecutableContentTransformerOptions(),file);
-    }
+	public static void main(String[] args) throws Exception {
+		ContentReader reader = new FileContentReader(new File("/home/rox/Costanza.docx"));
+		File fileSource = new File("/home/rox/Costanza.docx");
+		transformInternal(reader,new FileContentWriter(new File("/home/rox/Costanza.docx.pdf")),new RuntimeExecutableContentTransformerOptions(),fileSource);
+	}
 
-    static void transformInternal(ContentReader reader, ContentWriter writer, TransformationOptions options,File inputFile)
-            throws Exception {
+	static void transformInternal(ContentReader reader, ContentWriter writer, TransformationOptions options, File inputFile)
+			throws Exception {
 
-        OutputStream os = writer.getContentOutputStream();
-        String encoding = writer.getEncoding();
-        String targetMimeType = writer.getMimetype();
-        String sourceMimeType = reader.getMimetype();
+		String targetMimeType = writer.getMimetype();
+		String sourceMimeType = reader.getMimetype();
 
-        Writer ow = new OutputStreamWriter(os, encoding);
+		InputStream is = null;
 
-        InputStream is = null;
+		long startTime = 0;
 
-        long startTime = 0;
-        try {
-            is = reader.getContentInputStream();
+		is = reader.getContentInputStream();
 
-            URL obj = new URL(endpoint);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		Multipart part = new Multipart(DEFAULT_JODCONVERTER_ENDPOINT, "UTF-8");
+		part.addFilePart("inputFile", inputFile);
+		part.addFormFieldWithoutEnding("outputFormat", "pdf");
+
+		try {
+			part.finish(writer);
+		} catch (Exception e) {
+// Something went extremely wrong on remote server
+			logger.error("Remote transformation failed, remote host returned response code :" + part.getStatus());
+			if (logger.isDebugEnabled()) {
+				logger.debug("Source MimeType : " + sourceMimeType);
+				logger.debug("Target MimeType : " + targetMimeType);
+				logger.debug("Source size : " + reader.getSize());
+				logger.debug("Source ContentURL : " + reader.getContentUrl());
+				logger.debug("Remote JODConverter instance : " + endpoint);
+			}
+			throw new RuntimeException(e);
+
+		}finally {
+			if (logger.isDebugEnabled()) {
+			}
+
+			if (is != null) {
+				try {
+					is.close();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+		}
+	}
+
+/*    static void transformInternal(ContentReader reader, ContentWriter writer, TransformationOptions options)
+			throws Exception {
+
+		OutputStream os = writer.getContentOutputStream();
+		String encoding = writer.getEncoding();
+		String targetMimeType = writer.getMimetype();
+		String sourceMimeType = reader.getMimetype();
+
+		Writer ow = new OutputStreamWriter(os, encoding);
+
+		InputStream is = null;
+
+		long startTime = 0;
+		try {
+			is = reader.getContentInputStream();
+			if (logger.isDebugEnabled()) {
+				startTime = System.currentTimeMillis();
+			}
+
+			URL obj = new URL(endpoint);
+
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			// Set up limits -- TODO check if these values are taken into
+			// consideration? I am getting the feeling timeouts are handled
+			// on the dedicated thread for this transformation
+			long readLimitTimeMs = options.getReadLimitTimeMs();
+			if (readLimitTimeMs != -1)
+				con.setConnectTimeout((int) readLimitTimeMs);
+			long timeoutMs = options.getTimeoutMs();
+			if (timeoutMs != -1)
+				con.setReadTimeout((int) timeoutMs);
+
+			MultipartEntity multipartEntity = new MultipartEntity(HttpMultipartMode.STRICT);
+			multipartEntity.addPart("inputFile", new InputStreamBody(is, "bla bla")); // how to get real file name??
+			multipartEntity.addPart("outputFormat", new StringBody("pdf"));
+
+			// add request header
+			con.setRequestMethod("POST");
+			con.setRequestProperty("Content-Type", multipartEntity.getContentType().getValue());
+
+			// Send post request
+			con.setDoOutput(true);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+
+*//*			OutputStream out = con.getOutputStream();
+			try {
+				multipartEntity.writeTo(out);
+			} finally {
+				out.close();
+			}*//*
 
 
-            Multipart part = new Multipart(DEFAULT_JODCONVERTER_ENDPOINT,"UTF_8");
-            part.addFilePart("inputFile",inputFile);
-            part.addHeaderField("outputFormat","pdf");
-            List<String> result = part.finish();
 
-            // add request header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("Content-Type", sourceMimeType);
-            con.setRequestProperty("Accept", targetMimeType);
-            //curl -XPOST -F inputFile=@Costanza.docx -F outputFormat='pdf' http://localhost:8080/converter --output aaa
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			// FIXME add support for 2GB+ content... Really ?
+			IOUtils.copy(is, wr);
+			wr.flush();
+			wr.close();
 
-            // FIXME add support for 2GB+ content... Really ?
-            IOUtils.copy(is, wr);
-            wr.flush();
-            wr.close();
+			int responseCode = con.getResponseCode();
+			if (responseCode != 200) {
+				// Something went extremely wrong on remote server
+				logger.error("Remote transformation failed, remote host returned response code :" + responseCode);
+				if (logger.isDebugEnabled()) {
+					logger.debug("Source MimeType : " + sourceMimeType);
+					logger.debug("Target MimeType : " + targetMimeType);
+					logger.debug("Source size : " + reader.getSize());
+					logger.debug("Source ContentURL : " + reader.getContentUrl());
+					logger.debug("Remote JODConverter instance : " + endpoint);
+				}
+				throw new TransformException(con.getResponseMessage());
+			}
 
-            int responseCode = con.getResponseCode();
-            if (responseCode != 200) {
-                // Something went extremely wrong on remote server
-                logger.error("Remote transformation failed, remote host returned response code :" + responseCode);
-                if (logger.isDebugEnabled()) {
-                    logger.debug("Source MimeType : " + sourceMimeType);
-                    logger.debug("Target MimeType : " + targetMimeType);
-                    logger.debug("Source size : " + reader.getSize());
-                    logger.debug("Source ContentURL : " + reader.getContentUrl());
-                    logger.debug("Remote JODConverter instance : " + endpoint);
-                }
-                throw new TransformException(con.getResponseMessage());
-            }
+			IOUtils.copy(con.getInputStream(), os);
+		} finally {
+			*//*if (logger.isDebugEnabled()) {
+				logger.debug(calculateMemoryAndTimeUsage(reader, startTime));
+			}*//*
 
-            IOUtils.copy(con.getInputStream(), os);
-        } finally {
-            if (logger.isDebugEnabled()) {
-            }
+			if (is != null) {
+				try {
+					is.close();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			}
+			if (ow != null) {
+				try {
+					ow.close();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+	}*/
 
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            if (ow != null) {
-                try {
-                    ow.close();
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (Throwable e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
+
 }
--- src/main/java/testConnectHTTPJod/Multipart.java
+++ src/main/java/testConnectHTTPJod/Multipart.java
@@ -4,23 +4,36 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.httpclient.util.HttpURLConnection;
+import org.alfresco.service.cmr.repository.ContentWriter;
 
 public class Multipart {
         private final String boundary;
         private static final String LINE_FEED = "\r\n";
         private HttpURLConnection httpConn;
         private String charset;
-        private OutputStream outputStream;
+
+    public OutputStream getOutputStream() {
+        return outputStream;
+    }
+
+    private OutputStream outputStream;
         private PrintWriter writer;
+
+    public int getStatus() {
+        return status;
+    }
+
+    private int status;
 
         /**
          * This constructor initializes a new HTTP POST request with content type
@@ -65,6 +78,17 @@ public class Multipart {
             writer.flush();
         }
 
+    public void addFormFieldWithoutEnding(String name, String value) {
+        writer.append("--" + boundary).append(LINE_FEED);
+        writer.append("Content-Disposition: form-data; name=\"" + name + "\"")
+                .append(LINE_FEED);
+        writer.append("Content-Type: text/plain; charset=" + charset).append(
+                LINE_FEED);
+        writer.append(LINE_FEED);
+        writer.append(value);
+        writer.flush();
+    }
+
         /**
          * Adds a upload file section to the request
          *
@@ -100,6 +124,32 @@ public class Multipart {
             writer.flush();
         }
 
+        public void addInputStreamPart(String fieldName, InputStream inputStream)
+                throws IOException {
+            String fileName = "dummy";
+            writer.append("--" + boundary).append(LINE_FEED);
+            writer.append(
+                    "Content-Disposition: form-data; name=\"" + fieldName
+                            + "\"; filename=\"" + fileName + "\"")
+                    .append(LINE_FEED);
+            writer.append(
+                    "Content-Type: "
+                            + URLConnection.guessContentTypeFromName(fileName))
+                    .append(LINE_FEED);
+            writer.append("Content-Transfer-Encoding: binary").append(LINE_FEED);
+            writer.append(LINE_FEED);
+            writer.flush();
+
+            byte[] buffer = new byte[4096];
+            int bytesRead = -1;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.flush();
+            writer.append(LINE_FEED);
+            writer.flush();
+        }
+
         /**
          * Adds a header field to the request.
          *
@@ -118,26 +168,18 @@ public class Multipart {
          * status OK, otherwise an exception is thrown.
          * @throws IOException
          */
-        public List<String> finish() throws IOException {
-            List<String> response = new ArrayList<String>();
+        public void finish(ContentWriter finalWriter) throws IOException {
             writer.append(LINE_FEED).flush();
             writer.append("--" + boundary + "--").append(LINE_FEED);
             writer.close();
 
             // checks server's status code first
-            int status = httpConn.getResponseCode();
+            status = httpConn.getResponseCode();
             if (status == HttpURLConnection.HTTP_OK) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        httpConn.getInputStream()));
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    response.add(line);
-                }
-                reader.close();
+                finalWriter.putContent(httpConn.getInputStream());
                 httpConn.disconnect();
             } else {
                 throw new IOException("Server returned non-OK status: " + status);
             }
-            return response;
         }
     }
